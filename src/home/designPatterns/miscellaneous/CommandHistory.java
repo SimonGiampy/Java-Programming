af5a485adfb08b03e7e3817ea47ed3e98dfd81ea
SimonGiampy <simongiampydev@gmail.com>
2020-12-23 22:34:04 +0100

    prototype design pattern implementation added on top of the others

--- src/home/designPatterns/miscellaneous/CommandHistory.java
+++ src/home/designPatterns/miscellaneous/CommandHistory.java
@@ -3,16 +3,16 @@ package home.designPatterns.miscellaneous;
 import java.util.ArrayList;
 
 /**
- * TODO: implement a class that copies the object to be memorized (Commands) and stores them in a backup array. Tip: use the Prototype Pattern
+ * stores a copy of the command objects in a single arraylist. This class communicates with Decider for the implementation of the Prototype pattern.
  */
 class CommandHistory {
 	
 	private ArrayList<Command> history;
 	
+	//list initialization
 	protected CommandHistory() {
 		history = new ArrayList<>();
 	}
-	
 	
 	protected void push(Command command) {
 		history.add(command);
@@ -21,7 +21,5 @@ class CommandHistory {
 	protected void pop() {
 		if (!history.isEmpty()) history.remove(0);
 	}
-
-
 
 }
